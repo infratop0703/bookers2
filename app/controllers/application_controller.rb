e5d98903999967c5ac230f5e209a8e3bf674ccc3
EC2 Default User <ec2-user@ip-172-31-14-67.ec2.internal>
2022-09-03 04:22:37 +0000

    [Update]編集アイコン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    top_path
+    root_path
   end
 
   def get_profile_image(width, height)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'homes#top' , as: "top"
+  root to: 'homes#top'
   get "home/about" => "homes#about"
   devise_for :users
 
   resources :books, only: [:new,:create,:show,:destroy,:index,:edit,:update]
-  resources :users, only: [:show,:edit,:update,:index,:update,:destroy]
+  resources :users, only: [:show,:edit,:update,:index,:update,:destroy,:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
