0b669acb26c9d10f5ac0ce1d6a83848d003b118c
EC2 Default User <ec2-user@ip-172-31-14-67.ec2.internal>
2022-09-03 05:43:23 +0000

    [Finish]完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top,:about]
   
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :authenticate_user!, except: [:top,:about]
+  
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
   end
 
   def after_sign_out_path_for(resource)
     root_path
-  end
-
-  def get_profile_image(width, height)
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  end
-  profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,24 @@
 class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
-  
+
 
   def show
-    @book_new = Book.new
     @user = User.find(params[:id])
-    @books =@user.books
+    @books = Book.all
+    @book_new = Book.new
   end
-  
+
   def index
     @user = current_user
     @users = User.all
-    @book_new = Book.new
+    @book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
@@ -33,12 +36,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image,:introduction)
   end
-  
+
   def correct_user
     @user = User.find(params[:id])
-    if current_user != @user
-     redirect_to user_path(@user)
-    end
+    redirect_to(user_url(current_user)) unless @user == current_user
   end
 
 end
